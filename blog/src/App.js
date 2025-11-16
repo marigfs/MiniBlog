4eb105c2c55ae763b9b0633e4123e883c5b3fb99
Mariana Sales <mariana.sales2@estudante.ufla.br>
2025-11-16 20:15:18 -0300

    :sparkles: Primerio commit

---
+++ blog/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+//context
+import { AuthProvider } from "./context/AuthContext";
+
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ blog/src/hooks/useAuthentication.js
@@ -0,0 +1,77 @@
+import { db } from "../firebase/config";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  //cleanup
+  //deal with memory leaks
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha deve ter no mínimo 6 caracteres";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "Email já cadastrado";
+      } else {
+        systemErrorMessage = "Erro ao criar usuário";
+      }
+
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
