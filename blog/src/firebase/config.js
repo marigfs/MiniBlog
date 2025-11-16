cd2356ae912f4ef7ae43fc28eb9dceecd6cd09b2
Mariana Sales <97259775+marigfs@users.noreply.github.com>
2025-11-16 20:41:06 -0300

    :sparkles: Primeiro commit

--- blog/src/firebase/config.js
+++ blog/src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCup1USot0Ra5TAbb3BHks1kqoksT70rB0",
-  authDomain: "miniblog-1d786.firebaseapp.com",
-  projectId: "miniblog-1d786",
-  storageBucket: "miniblog-1d786.appspot.com",
-  messagingSenderId: "659075615437",
-  appId: "1:659075615437:web:a9a27f8a05e41a613d7784",
+  apiKey: "-",
+  authDomain: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-",
+  appId: "-",
 };
 
 const app = initializeApp(firebaseConfig);
